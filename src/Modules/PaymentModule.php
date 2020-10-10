7e2012482bafd7dc7717a04654dab6c6ab159824
Arushad Ahmed <dash-8x@hotmail.com>
2020-10-10 15:48:40 +0500

    - Added checkout url

--- src/Modules/PaymentModule.php
+++ src/Modules/PaymentModule.php
@@ -4,6 +4,8 @@ namespace Dash8x\RevenueMonster\Modules;
 
 class PaymentModule extends \RevenueMonster\SDK\Modules\PaymentModule
 {
+    const CHECKOUT_PRODUCTION_ENDPOINT = 'https://pg.revenuemonster.my/v1/checkout?checkoutId=';
+
     /**
      * Get a web payment by the checkout ID
      *
@@ -16,5 +18,16 @@ class PaymentModule extends \RevenueMonster\SDK\Modules\PaymentModule
     {
         $uri = $this->getOpenApiUrl('v3', "/payment/online?checkoutId=$checkoutId");
         return $this->mapResponse($this->callApi('get', $uri)->send());
+    }
+
+    /**
+     * Get the url for a web payment checkout
+     *
+     * @param $checkoutId
+     * @return string
+     */
+    public function getCheckoutUrl(string $checkoutId)
+    {
+        return self::CHECKOUT_PRODUCTION_ENDPOINT . $checkoutId;
     }
 }
